0406eb3464421c28235824586d74e7f68e81db69
maxwen <max.weninger@gmail.com>
2019-02-03 15:23:51 +0530

    Legion:QS header image [1/2]

--- src/com/legion/settings/fragments/QuickSettings.java
+++ src/com/legion/settings/fragments/QuickSettings.java
@@ -22,7 +22,10 @@ import android.os.Bundle;
 import android.os.UserHandle;
 import android.provider.SearchIndexableResource;
 import android.provider.Settings;
+import android.content.pm.PackageManager;
 
+import android.content.ContentResolver;
+import android.content.res.Resources;
 import androidx.preference.ListPreference;
 import androidx.preference.SwitchPreference;
 import androidx.preference.Preference;
@@ -37,20 +40,48 @@ import com.android.settingslib.search.SearchIndexable;
 
 import com.android.internal.logging.nano.MetricsProto;
 
+import com.legion.settings.preferences.SystemSettingMasterSwitchPreference;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Locale;
+import android.text.TextUtils;
+import android.view.View;
 
 @SearchIndexable
-public class QuickSettings extends SettingsPreferenceFragment {
+public class QuickSettings extends SettingsPreferenceFragment implements
+        OnPreferenceChangeListener {
+
+    private static final String STATUS_BAR_CUSTOM_HEADER = "status_bar_custom_header";
+
+    private SystemSettingMasterSwitchPreference mCustomHeader;
 
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
         addPreferencesFromResource(R.xml.legion_settings_quicksettings);
         PreferenceScreen prefSet = getPreferenceScreen();
+        ContentResolver resolver = getActivity().getContentResolver();
+
+        mCustomHeader = (SystemSettingMasterSwitchPreference) findPreference(STATUS_BAR_CUSTOM_HEADER);
+        int qsHeader = Settings.System.getInt(resolver,
+                Settings.System.STATUS_BAR_CUSTOM_HEADER, 0);
+        mCustomHeader.setChecked(qsHeader != 0);
+        mCustomHeader.setOnPreferenceChangeListener(this);
 
     }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        ContentResolver resolver = getActivity().getContentResolver();
+		if (preference == mCustomHeader) {
+            boolean header = (Boolean) newValue;
+            Settings.System.putInt(resolver,
+                    Settings.System.STATUS_BAR_CUSTOM_HEADER, header ? 1 : 0);
+            return true;
+    }
+	return false;
+  }
 
     @Override
     public int getMetricsCategory() {
