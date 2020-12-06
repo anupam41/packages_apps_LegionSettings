88d43b0e921a2441a8f8e07a5ac297e6f1c8b4bb
703joko <703joko@gmail.com>
2020-12-07 05:10:58 +0700

    Legion:Add panelbg style [2/3]
    Signed-off-by: Genkzsz11 <genkzsz11@nusantaradevs.org>

--- src/com/legion/settings/fragments/Themes.java
+++ src/com/legion/settings/fragments/Themes.java
@@ -64,6 +64,7 @@ public class Themes extends DashboardFragment implements
     private static final String TAG = "Themes";
     private static final String BRIGHTNESS_SLIDER_STYLE = "brightness_slider_style";
     private static final String UI_STYLE = "ui_style";
+    private static final String PREF_PANEL_BG = "panel_bg";
     private static final String ACCENT_COLOR = "accent_color";
     private static final String ACCENT_COLOR_PROP = "persist.sys.theme.accentcolor";
     private static final String GRADIENT_COLOR = "gradient_color";
@@ -83,6 +84,7 @@ public class Themes extends DashboardFragment implements
     private CustomSeekBarPreference mQsPanelAlpha;
     private ListPreference mBrightnessSliderStyle;
     private ListPreference mUIStyle;
+    private ListPreference mPanelBg;
 
     @Override
     protected String getLogTag() {
@@ -159,6 +161,16 @@ public class Themes extends DashboardFragment implements
                 return false;
             }
        });
+
+       mPanelBg = (ListPreference) findPreference(PREF_PANEL_BG);
+        int mPanelValue = getOverlayPosition(ThemesUtils.PANEL_BG_STYLE);
+        if (mPanelValue != -1) {
+                mPanelBg.setValue(String.valueOf(mPanelValue + 2));
+        } else {
+                mPanelBg.setValue("1");
+              }
+        mPanelBg.setSummary(mPanelBg.getEntry());
+        mPanelBg.setOnPreferenceChangeListener(this);
 
         mUiModeManager = getContext().getSystemService(UiModeManager.class);
 
@@ -313,6 +325,21 @@ public class Themes extends DashboardFragment implements
                  mOverlayService.reloadAssets("com.android.systemui", UserHandle.USER_CURRENT);
              } catch (RemoteException ignored) {
              }
+            } else if (preference == mPanelBg) {
+                String panelbg = (String) objValue;
+                int panelBgValue = Integer.parseInt(panelbg);
+                mPanelBg.setValue(String.valueOf(panelBgValue));
+                String overlayName = getOverlayName(ThemesUtils.PANEL_BG_STYLE);
+                    if (overlayName != null) {
+                        handleOverlays(overlayName, false, mOverlayService);
+                    }
+                    if (panelBgValue > 1) {
+                        LegionUtils.showSystemUiRestartDialog(getContext());
+                        handleOverlays(ThemesUtils.PANEL_BG_STYLE[panelBgValue -2],
+                                true, mOverlayService);
+    
+                }
+                mPanelBg.setSummary(mPanelBg.getEntry());
         }
         return true;
     }
