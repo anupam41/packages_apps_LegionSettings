0b75e039fb944f789167b144ca882ea06e455206
anonhacker47 <rubeenbilal47@gmail.com>
2021-05-22 15:20:28 +0000

    Fixup: LegionSettings Redesign. *Remove Actionbar for now *Add Expanded header titles for customizations

--- src/com/legion/settings/LegionSettings.java
+++ src/com/legion/settings/LegionSettings.java
@@ -24,6 +24,8 @@ import android.app.Activity;
 import android.content.pm.ActivityInfo;
 import android.content.res.Configuration;
 import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.Surface;
 import android.preference.Preference;
@@ -31,6 +33,10 @@ import android.widget.GridLayout;
 import android.widget.GridView;
 import android.widget.Toast;
 import android.view.View;
+import android.view.Window;  
+import android.view.WindowManager;  
+import android.view.WindowManager.LayoutParams;
+import com.android.internal.policy.DecorView;
 import android.util.Log;
 
 import android.view.LayoutInflater;
@@ -80,6 +86,9 @@ public class LegionSettings extends SettingsPreferenceFragment implements View.O
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		    getActivity().getActionBar().hide();
+                    getActivity().getWindow().setStatusBarColor(Color.TRANSPARENT);
+		    getActivity().getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
 
        return inflater.inflate(R.layout.legion_settings, container, false);
     }
@@ -87,8 +96,6 @@ public class LegionSettings extends SettingsPreferenceFragment implements View.O
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-		    getActivity().getActionBar().hide();
-                   getActivity().getWindow().setStatusBarColor(Color.TRANSPARENT);
 
         mlegionqsCard = (MaterialCardView) view.findViewById(R.id.legionqs_card);
         mlegionqsCard.setOnClickListener(this);
